15a3bde024c165d33305639536ff84d3a96c56c6
Kaique Devesa <kaiquedeveza1@hotmail.com>
2025-02-18 21:50:27 -0300

    adicionando script button

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const btnAdd = document.querySelectorAll(".btn-add");
+const btnAdd = document.querySelectorAll(".container-btn");
+let count = 0;
+
+function handleButtonClick(button) {
+  button.classList.add("click");
+  count++;
+  console.log(count);
+}
 
 btnAdd.forEach((button) => {
   button.addEventListener("click", function () {
-    console.log("adicionei");
+    handleButtonClick(button);
   });
+
+  button.classList.remove("click");
 });
