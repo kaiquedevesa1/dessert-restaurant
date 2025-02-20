a8c802f3896bc8bdd4277a27f79605989e3447c5
Kaique Devesa <kaiquedeveza1@hotmail.com>
2025-02-19 23:16:07 -0300

    estilizando os botoes

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-let count = 0;
-
 function handleClick(e) {
+  // Seleciona todos os botões
   const btnAdd = document.querySelectorAll(".container-btn");
 
-  btnAdd.forEach((add) => {
-    add.classList.remove("click");
-  });
+  // Remove a classe “click” de todos
 
+  // Adiciona a classe “click” ao botão atual
   e.currentTarget.classList.add("click");
 
-  let eClick = document.querySelectorAll("add-cart");
+  // Obtenha o contador atual do botão clicado ou inicialize em 0
 
-  eClick.innerHTML = `<div>${count}</div>`;
+  // Atualiza o HTML do botão com o novo contador
+  e.currentTarget.innerHTML = `<div class='add-item'>
+    <div class= 'container-decrement'>
+        <img  class='decrement' src='./image/icon-decrement-quantity.svg'/>
+    </div>
+    <div>
+      1
+    </div>
+    
+    <div class='container-increment'>
+      <img class="increment" src="./image/icon-increment-quantity.svg" />
+       
+      </div>
+  <div/>`;
 }
 
+// Seleciona todos os botões e adiciona o evento de click
 const btnAdd = document.querySelectorAll(".container-btn");
 
 btnAdd.forEach((add) => {
